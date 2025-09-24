117818c0a3d81c66dd500ec11ded49b06690d05a
shri-prakhar <23f3000621@es.study.iitm.ac.in>
2025-09-24 11:23:50 +0530

    added changes in  spinning to nodes when running

--- n8n/apps/web/components/nodes/spinOverlay.tsx
+++ n8n/apps/web/components/nodes/spinOverlay.tsx
@@ -1,9 +1,9 @@
 export default function SpinnerOverlay() {
   return (
-    <div className="absolute inset-0 z-20 bg-black/30 backdrop-blur-sm flex items-center justify-center rounded-lg">
+    <div className="absolute inset-0 z-20 bg-orange-300/20 backdrop-blur-sm flex items-center justify-center rounded-lg">
       <div className="relative w-12 h-12">
         <div className="absolute inset-0 rounded-full border-4 border-t-transparent border-white animate-spin"></div>
-        <div className="absolute inset-2 rounded-full border-4 border-t-transparent border-pink-400 animate-spin-slow"></div>
+        <div className="absolute inset-2 rounded-full border-4 border-t-transparent border-pink-400 animate-spin"></div>
       </div>
     </div>
   );
